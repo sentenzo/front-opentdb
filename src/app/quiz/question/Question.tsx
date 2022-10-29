156cffd779857b28fd4a2184d0c18473ecc4ead6
sentenzo <kassiyskley@rambler.ru>
2022-10-29 16:23:26 +0300

    styling `Question` component

--- src/app/quiz/question/Question.tsx
+++ src/app/quiz/question/Question.tsx
@@ -5,17 +5,19 @@ const Question = () => {
     const qname = nanoid();
     return (
         <div className="question">
-            <h2>
+            <h3>
                 Question text?
-            </h2>
-            <input type="radio" name={qname} id={`q1_${qname}`} />
-            <label htmlFor={`q1_${qname}`}>option 1</label>
-            <input type="radio" name={qname} id={`q2_${qname}`} />
-            <label htmlFor={`q2_${qname}`}>option 2</label>
-            <input type="radio" name={qname} id={`q3_${qname}`} />
-            <label htmlFor={`q3_${qname}`}>option 3</label>
-            <input type="radio" name={qname} id={`q4_${qname}`} />
-            <label htmlFor={`q4_${qname}`}>option 4</label>
+            </h3>
+            <div className="options">
+                <input type="radio" name={qname} id={`q1_${qname}`} />
+                <label htmlFor={`q1_${qname}`}>option 1</label>
+                <input type="radio" name={qname} id={`q2_${qname}`} />
+                <label htmlFor={`q2_${qname}`}>option 2</label>
+                <input type="radio" name={qname} id={`q3_${qname}`} />
+                <label htmlFor={`q3_${qname}`}>option 3</label>
+                <input type="radio" name={qname} id={`q4_${qname}`} />
+                <label htmlFor={`q4_${qname}`}>option 4</label>
+            </div>
         </div>
     )
 
